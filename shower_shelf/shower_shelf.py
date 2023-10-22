ecc9ca3f111228c5a85144bacc4790b29cfbcc72
janeirik <>
2023-10-22 14:59:14 +0200

    final design

--- shower_shelf/shower_shelf.py
+++ shower_shelf/shower_shelf.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-wall_thickness = 2.
+wall_thickness = 3.
 wall_height = 30.
 
 bottom_x = 156
-bottom_y = 156
+bottom_y = 70
 rect_center = (bottom_x / 2, bottom_y / 2)
 d = 10.                # minimal diameter
 D = 2 / np.sqrt(3) * d # maximal diameter
@@ -46,48 +46,22 @@ walls = cq.Workplane(origin=(-wall_thickness, -wall_thickness, 0))\
         .rect(bottom_x, bottom_y, centered=False)\
         .extrude(wall_height)\
 
-
-glass_thickness = 5
-
-# connector = cq.Workplane()\
-#         .polygon(6,D+t)\
-#         .extrude(2)
-# 
-# cut = cq.Workplane(origin=(0,-D,0))\
-#         .rect(-2*D, 2*D, centered=False)\
-#         .extrude(2)
-# 
-# connector = connector.cut(cut)
-
-a = 0.5*t - wall_thickness - glass_thickness - 0.5 * d - 5
-b = 0.5*t - wall_thickness - glass_thickness - 0.5 * d
-c = 0.5*t - wall_thickness - 0.5 * d
-hook = cq.Workplane("YZ")\
-        .move(-1,0)\
-        .lineTo(-1, 100)\
-        .lineTo(a, 100)\
-        .lineTo(a, 80)\
-        .lineTo(b, 80)\
-        .lineTo(b, 95)\
-        .lineTo(c, 95)\
-        .lineTo(c,wall_height+5)\
-        .lineTo(0.5 * (t - d), wall_height+5)\
-        .lineTo(0.5 * (t - d), -4.1 - 5)\
-        .lineTo(20, -4.1 - 5)\
-        .lineTo(20, -4.1)\
-        .lineTo(-1, -4.1)\
+eps = 0.5 
+hook = cq.Workplane("YZ", origin=(-wall_thickness, 0, 0))\
+        .lineTo(0, wall_height * 2)\
+        .lineTo(-13, wall_height * 2)\
+        .lineTo(-13, wall_height+15)\
+        .lineTo(-wall_thickness-6-eps, wall_height+15)\
+        .lineTo(-wall_thickness-6-eps, wall_height + 25)\
+        .lineTo(-wall_thickness, wall_height + 25)\
+        .lineTo(-wall_thickness,0)\
         .close()\
-        .extrude(3.5)
-
-#connector = connector.union(hook)
-
-hook = hook\
-        .translate((10, 5, 4))
+        .extrude(bottom_x + 2 * wall_thickness)
 
 
+shelf = bottom_plate.union(walls).union(hook)
 
 
-show_object(walls)
-show_object(bottom_plate)
-show_object(hook)
+cq.exporters.export(shelf, "shelf.stl")
+show_object(shelf)
 
